a8d1c0aa2c44cbe295bdeda14ab2b45ab93dfefc
Tyrese-Mv <musawenkosi106@gmail.com>
2025-04-09 18:12:37 +0200

    basic movie website

---
+++ react-tutorial/src/services/api.js
@@ -0,0 +1,14 @@
+const API_KEY = "b8946ba3842c6cf3dc507d586b165072";
+const BASE_URL = "https://api.themoviedb.org/3"
+
+export const getPopularMovies = async() => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json();
+    return data.results
+}
+
+export const searchMovies = async(query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = await response.json();
+    return data.results
+}
